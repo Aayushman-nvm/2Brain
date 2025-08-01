4e74ca0b3040f3cac3e9bf42bfbfbbde36495041
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-08-01 18:12:19 +0530

    content type added

--- server/src/model/content.ts
+++ server/src/model/content.ts
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 const contentSchema = new mongoose.Schema({
   title: { type: String, required: true },
   link:{type:String},
+  type:{type:String,enum:["Image","Video","Tweet","WebSite"]},
   madeBy: { type: mongoose.Schema.ObjectId, ref: "Users", required: true },
-  tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "tags" }],
+  tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tags" }],
 });
 
 export const Contents = mongoose.model("contents", contentSchema);
