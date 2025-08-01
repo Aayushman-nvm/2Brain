54e32b8c627bbcc926e93b479a8b519bc5599474
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-08-01 18:11:25 +0530

    Few routes added

---
+++ server/src/router/serverRoutes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { handleLogin, handleLogout, handleRegister } from "../controller/auth";
+import {
+  handleContents,
+  getContents,
+  deleteContent,
+} from "../controller/content";
+
+export const router = Router();
+
+router.post("/login", handleLogin);
+router.post("/register", handleRegister);
+router.post("/logout", handleLogout);
+
+router.post("/content", handleContents);
+router.get("/content", getContents);
+router.delete("/content", deleteContent);
+
+/*router.post("/sharelink");
+router.get("/sharelink/:linkId");*/
