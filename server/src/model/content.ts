3d5443141b0a8863d2042289f2ebfce2be05dead
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-08-02 19:04:09 +0530

    miscellaneous type

--- server/src/model/content.ts
+++ server/src/model/content.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const contentSchema = new mongoose.Schema({
   title: { type: String, required: true },
   link:{type:String},
-  type:{type:String,enum:["Image","Video","Tweet","WebSite"]},
+  type:{type:String,enum:["Image","Video","Tweet","WebSite","Miscellaneous"]},
   madeBy: { type: mongoose.Schema.ObjectId, ref: "Users", required: true },
   tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tags" }],
 });
