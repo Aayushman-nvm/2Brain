2e011a79d9ac0b6170c76cf420ffea3d7b22b715
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-08-02 18:11:11 +0530

    Links controller

---
+++ server/src/controller/link.ts
@@ -0,0 +1,56 @@
+import { nanoid } from "nanoid";
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+
+import { Links } from "../model/link";
+import { Contents } from "../model/content";
+
+export async function handleShare(req: Request, res: Response) {
+  const body = req.body as { share: boolean; id: string };
+
+  try {
+    if (!body || typeof body.share !== "boolean" || !body.id) {
+      return res.status(400).json({ message: "Invalid request body" });
+    }
+
+    if (body.share) {
+      const shareLink = await Links.create({
+        userId: new mongoose.Types.ObjectId(body.id),
+        hash: nanoid(5),
+      });
+
+      return res.status(201).json({shareLink:shareLink, link:"/sharelink/"+shareLink.hash});
+    } else {
+      await Links.deleteOne({
+        userId: new mongoose.Types.ObjectId(body.id),
+      });
+      return res.status(201).json({ message: "Deleted shareable link" });
+    }
+  } catch (error) {
+    console.error("Error creating share link:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function getLink(req: Request, res: Response) {
+  try {
+    const { linkId } = req.params;
+
+    const linkDoc = await Links.findOne({ linkId });
+    if (!linkDoc) {
+      return res.status(404).json({ message: "Invalid share link" });
+    }
+
+    const content = await Contents.findOne({ madeBy: linkDoc.userId });
+    if (!content) {
+      return res
+        .status(404)
+        .json({ message: "No content found for this link" });
+    }
+
+    res.json(content);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+}
