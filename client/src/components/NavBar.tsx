9a2dbb8709bc7ced222e5309cafa6657a1915474
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-08-06 20:42:03 +0530

    Share Link

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,12 +1,26 @@
 import Button from "./ui/Button";
-import { togglePopup } from "../states/slice";
-import { useDispatch } from "react-redux";
+import { togglePopup, setShare } from "../states/slice";
+import { useSelector, useDispatch } from "react-redux";
 
 function NavBar() {
   const dispatch = useDispatch();
+  const apiUrl = import.meta.env.VITE_SERVER_URL;
+  const userId = useSelector((state: any) => state.app.user._id);
+  const isShare=useSelector((state: any) => state.app.share);
 
   async function handleShare() {
     try {
+      dispatch(setShare());
+      const response=await fetch(`${apiUrl}/sharelink`,{
+        method:"POST",
+        headers: { "Content-Type": "application/json" },
+        body:JSON.stringify({share:!isShare,id:userId}),
+      });
+      const data=await response.json();
+      console.log(data);
+      if(!isShare===true){alert(`Share link: ${apiUrl}${data.link}`)}
+      else{alert(`Share link: ${data.message}`)}
+      
     } catch (error) {
       console.log(error);
     }
