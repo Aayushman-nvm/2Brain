b7ad5c215f437a5076e8d4e61798b0d5f730bdd2
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-08-06 20:45:16 +0530

    Tweet

---
+++ client/src/pages/Tweet.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+import Card from "../components/Card";
+
+function Tweet() {
+  interface ContentItem {
+  title: string,
+  link: string,
+  type: "Image" | "Video" | "Tweet" | "WebSite" | "Miscellaneous",
+  _id: string,
+  madeBy: string
+}
+
+  const [content, setContent] = useState<ContentItem[]>([]);
+  const userId = useSelector((state: any) => state.app.user._id);
+  const apiUrl = import.meta.env.VITE_SERVER_URL;
+
+  async function getContent() {
+    try {
+      const response = await fetch(`${apiUrl}/content?userId=${userId}`, {
+        method: "GET",
+      });
+      const data = await response.json();
+      setContent(data);
+    } catch (err) {
+      console.error("Error fetching content:", err);
+    }
+  }
+
+  useEffect(() => {
+    getContent();
+  }, []);
+  return (
+    <div>
+      Tweet
+      {content
+        .filter((item) => item.type === "Tweet")
+        .map((item, index) => (
+          <Card key={index} title={item.title} link={item.link} type={item.type} id={item._id} madeBy={item.madeBy}/>
+        ))}
+    </div>
+  );
+}
+
+export default Tweet;
