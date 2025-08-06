a7e1542f6441a93a296b22e449cec190c454a44f
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-08-06 20:45:00 +0530

    Image

--- client/src/pages/Image.tsx
+++ client/src/pages/Image.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+import Card from "../components/Card";
+
 function Image() {
-  return (
-    <div>Image</div>
-  )
+  interface ContentItem {
+  title: string,
+  link: string,
+  type: "Image" | "Video" | "Tweet" | "WebSite" | "Miscellaneous",
+  _id: string,
+  madeBy: string
 }
 
-export default Image
+  const [content, setContent] = useState<ContentItem[]>([]);
+  const userId = useSelector((state: any) => state.app.user._id);
+  const apiUrl = import.meta.env.VITE_SERVER_URL;
+
+  async function getContent() {
+    try {
+      const response = await fetch(`${apiUrl}/content?userId=${userId}`, {
+        method: "GET",
+      });
+      const data = await response.json();
+      setContent(data);
+    } catch (err) {
+      console.error("Error fetching content:", err);
+    }
+  }
+
+  useEffect(() => {
+    getContent();
+  }, []);
+
+  return (
+    <div>
+      <h2>Images</h2>
+      {content
+        .filter((item) => item.type === "Image")
+        .map((item, index) => (
+          <Card key={index} title={item.title} link={item.link} type={item.type} id={item._id} madeBy={item.madeBy}/>
+        ))}
+    </div>
+  );
+}
+
+export default Image;
